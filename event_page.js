785818051953e6e1d4f7600b1f1d440997ed9295
Stuart Kent <skent.dev@gmail.com>
2017-01-20 12:12:59 -0500

    Misc cleanup

--- event_page.js
+++ event_page.js
@@ -18,13 +18,13 @@ function handleBrowserActionIconClick() {
 	// The new state we are transitioning to. Correctly handles no stored data. Extension manifest
 	// does not need to declare the storage permission since we are leveraging the HTML5 storage APIs.
 	// Note: we can only store strings using these APIs!
-	var targetEnabledState = localStorage.getItem(hidingEnabledKey) !== trueString
+	var targetHidingEnabledState = localStorage.getItem(hidingEnabledKey) !== trueString
 
 	// Write the new state to storage.
-	localStorage.setItem(hidingEnabledKey, targetEnabledState ? trueString : falseString)
+	localStorage.setItem(hidingEnabledKey, targetHidingEnabledState ? trueString : falseString)
 
 	// Update the browser action icon to reflect the new state.
-	updateBrowserActionIcon(targetEnabledState)
+	updateBrowserActionIcon(targetHidingEnabledState)
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
